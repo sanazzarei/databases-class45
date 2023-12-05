e41fd07a372ae7d99981377e537ae03b158e6905
sanaz <sanazzarei1995@gmail.com>
2023-12-05 15:53:08 +0100

    update assignment

--- Week3/asignments/exercise2/transaction.js
+++ Week3/asignments/exercise2/transaction.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "hyfuser",
   password: "hyfpassword",
-  database: trans_database,
+  database: "trans_database",
 });
 
 connection.beginTransaction((err) => {
